d163f7b3b3b7f79a1d96d4e1b89bef1dcb71364b
Manokero <wildinmota@gmail.com>
2019-07-14 15:18:02 -0400

    Database connect

--- plaisi_api/settings.py
+++ plaisi_api/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'plaisi_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_file_default': 'config/db/connection.cnf'
+        },
     }
 }
 
