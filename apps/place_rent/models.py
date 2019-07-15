da8b676697264f6e804b811115c337a7d4120957
Manokero <wildinmota@gmail.com>
2019-07-15 01:22:16 -0400

    WIP

--- apps/place_rent/models.py
+++ apps/place_rent/models.py
@@ -1,6 +1,24 @@
 from django.db import models
 
 # Create your models here.
+SD = 'SD'
+SC = 'SC'
+SA = 'SA'
 
-class PlaceRent(models.Model):
-    pass
+CITIES_DOMINICAN_REPUBLIC = [
+    (SD, 'Santo Domingo'),
+    (SC, 'San Cristobal'),
+    (SA, 'Santiago'),
+]
+
+class HouseRent(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=1000)
+    bedroom_number = models.SmallIntegerField()
+    bathroom_number = models.SmallIntegerField()
+    construcction_size = models.IntegerField()
+    solar_size = models.IntegerField()
+    price = models.FloatField()
+    city = models.CharField(max_length=4, choices=YEAR_IN_SCHOOL_CHOICES, default=FRESHMAN)
+    # photos
+    
